5e11ea86e0f04f1bc4d227aacbc5aba27462c5ed
smallchill <smallchill@163.com>
2019-10-28 11:02:43 +0800

    :zap: 增加keepalive，切换tab后保留页面状态

--- src/page/index/index.vue
+++ src/page/index/index.vue
@@ -18,9 +18,9 @@
         <!-- 主体视图层 -->
         <div style="height:100%;overflow-y:auto;overflow-x:hidden;" id="avue-view" v-show="!isSearch">
           <keep-alive>
-            <router-view class="avue-view" v-if="$route.meta.$keepAlive" />
+            <router-view class="avue-view" v-if="$route.meta.$keepAlive || $route.meta.$keepAlive === undefined"/>
           </keep-alive>
-          <router-view class="avue-view" v-if="!$route.meta.$keepAlive" />
+          <router-view class="avue-view" v-if="!$route.meta.$keepAlive  && $route.meta.$keepAlive !== undefined"/>
         </div>
       </div>
     </div>
--- src/page/index/layout.vue
+++ src/page/index/layout.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
     <keep-alive>
-      <router-view class="avue-view"
-                   v-if="$route.meta.$keepAlive" />
+      <router-view class="avue-view" v-if="$route.meta.$keepAlive || $route.meta.$keepAlive === undefined" />
     </keep-alive>
-    <router-view class="avue-view"
-                 v-if="!$route.meta.$keepAlive" />
+    <router-view class="avue-view" v-if="!$route.meta.$keepAlive  && $route.meta.$keepAlive !== undefined" />
   </div>
 </template>
