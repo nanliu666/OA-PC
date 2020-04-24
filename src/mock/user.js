553e88c8e600efb4b1f0f23f719bf0f127cc26e6
liukangqi <liukangqi56@hotmail.com>
2020-04-24 17:13:14 +0800

    fix: 修复

--- src/mock/user.js
+++ src/mock/user.js
@@ -1,45 +1,40 @@
 import Mock from 'mockjs'
 export default ({ mock }) => {
   if (!mock) return
-  // 用户登录
-  Mock.mock('/user/login', 'post', {
-    data: new Date().getTime() + ''
-  })
-  //用户退出
-  Mock.mock('/user/logout', 'get', {
-    data: true
-  })
-  //刷新token
-  Mock.mock('/user/refesh', 'post', {
-    data: new Date().getTime() + ''
-  })
+  // // 用户登录
+  // Mock.mock('/user/login', 'post', {
+  //   data: new Date().getTime() + ''
+  // })
+  // //用户退出
+  // Mock.mock('/user/logout', 'get', {
+  //   data: true
+  // })
+  // //刷新token
+  // Mock.mock('/user/refesh', 'post', {
+  //   data: new Date().getTime() + ''
+  // })
 
   //获取表格数据
-  Mock.mock('/user/getTable', 'get', () => {
+  Mock.mock(new RegExp('/api/org/v1/user/tag/define' + '.*'), 'get', () => {
     let list = []
     for (let i = 0; i < 5; i++) {
       list.push(
         Mock.mock({
           id: '@increment',
-          name: Mock.mock('@cname'),
-          username: Mock.mock('@last'),
-          type: [0, 2],
-          checkbox: [0, 1],
-          'number|0-100': 0,
-          datetime: 1532932422071,
-          'sex|0-1': 0,
-          moreselect: [0, 1],
-          grade: 0,
-          address: Mock.mock('@cparagraph(1, 3)'),
-          check: [1, 3, 4]
+          name: '@cname',
+          'userNum|0-100': 0,
+          createTime: '@date',
+          updateTime: '@date'
         })
       )
     }
     return {
-      data: {
-        total: 11,
-        pageSize: 10,
-        tableData: list
+      resCode: 200,
+      resMsg: '描述',
+      response: {
+        totalNum: 5,
+        totalPage: 5,
+        data: list
       }
     }
   })
