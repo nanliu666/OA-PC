b359d77fc66b30421e3778a70944e90ee57f0b92
liukangqi <liukangqi56@hotmail.com>
2020-04-28 21:04:57 +0800

    chore: 添加开发版依赖

--- vue.config.js
+++ vue.config.js
@@ -10,13 +10,16 @@ module.exports = {
   productionSourceMap: false,
   chainWebpack: (config) => {
     //忽略的打包文件
-    config.externals({
-      vue: 'Vue',
-      'vue-router': 'VueRouter',
-      vuex: 'Vuex',
-      axios: 'axios',
-      'element-ui': 'ELEMENT'
-    })
+    if (isProduction) {
+      config.externals({
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex',
+        axios: 'axios',
+        'element-ui': 'ELEMENT'
+      })
+    }
+
     const entry = config.entry('app')
     entry.add('babel-polyfill').end()
     entry.add('classlist-polyfill').end()
