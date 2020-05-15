d41d9f352081461a993626775c7c3ec755d91834
fenda <1274678042@qq.com>
2020-05-15 13:51:33 +0800

    fix: 修改样式和角色权限修改

--- src/views/system/components/roleUserList.vue
+++ src/views/system/components/roleUserList.vue
@@ -5,7 +5,7 @@
       :visible.sync="dialogVisible"
       width="800px"
       :close-on-click-modal="false"
-      :modal-append-to-body="false"
+      append-to-body
     >
       <div v-loading="loading">
         <div style="text-align: right;margin-bottom: 10px;">
