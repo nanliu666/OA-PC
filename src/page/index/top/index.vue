72e1dba3cc5ae135954c4c69a417f09142baf86a
kangru <18392172902@163.com>
2020-05-26 15:31:35 +0800

    fix: 登陆以后右上角的个人中心入口展示

--- src/page/index/top/index.vue
+++ src/page/index/top/index.vue
@@ -110,11 +110,11 @@
               {{ $t('navbar.dashboard') }}
             </router-link>
           </el-dropdown-item>
-          <!-- <el-dropdown-item>
+          <el-dropdown-item>
             <router-link to="/info/index">
               {{ $t('navbar.userinfo') }}
             </router-link>
-          </el-dropdown-item> -->
+          </el-dropdown-item>
           <el-dropdown-item
             divided
             @click.native="logout"
