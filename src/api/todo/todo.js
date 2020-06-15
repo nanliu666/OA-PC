acb70f31dd32b1cce960192d4c69821638846817
dwq <d13189432359@163.com>
2020-06-15 09:51:16 +0800

    feat:  代办事项的面试详情和提交面试评价

--- src/api/todo/todo.js
+++ src/api/todo/todo.js
@@ -1,4 +1,4 @@
-import { get } from '@/router/axios'
+import { get, post } from '@/router/axios'
 
 /**
  * 待办任务查询接口
@@ -10,4 +10,13 @@ import { get } from '@/router/axios'
  * */
 export const getTodoList = (params) => {
   return get('/task/v1/todo/list', params)
+}
+// 候选人面试详情查询
+export const getInterviewInfo = (params) => {
+  return get('/user/v1/person/interview/info', params)
+}
+
+// 候选人面试详情评价提交
+export const postInterviewInfo = (params) => {
+  return post('/user/v1/person/interview/info', params)
 }
--- src/router/views/todo.js
+++ src/router/views/todo.js
@@ -8,6 +8,16 @@ export default [
         path: '/todo/todoList',
         name: '待办中心',
         component: () => import(/* webpackChunkName: "views" */ '@/views/todo/todoList')
+      },
+      {
+        path: '/todo/interviewDetail',
+        name: '面试详情',
+        component: () => import(/* webpackChunkName: "views" */ '@/views/todo/interviewDetail')
+      },
+      {
+        path: '/todo/interviewEdit',
+        name: '面试评价',
+        component: () => import(/* webpackChunkName: "views" */ '@/views/todo/interviewEdit')
       }
     ]
   }
