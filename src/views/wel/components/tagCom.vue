d757a8018f39e6644d1c7e4875e85c3c9aaacaab
dwq <d13189432359@163.com>
2020-06-29 15:32:49 +0800

    fix:  优化代码

--- src/views/wel/components/tagCom.vue
+++ src/views/wel/components/tagCom.vue
@@ -3,11 +3,18 @@
     class="wrap"
     :style="{ width }"
   >
-    <tagTitle
-      :title="title"
-      :icon="icon"
-      @click="handelClick"
-    />
+    <!-- 标题 -->
+    <div class="header">
+      <span class="title">
+        <span>{{ title }}</span>
+        <i
+          v-if="icon"
+          class="icon-arrow-right-outlined"
+          style="margin-right:12px;cursor: pointer; "
+        />
+      </span>
+    </div>
+    <!-- 内容 -->
     <el-tabs
       v-model="chooseName"
       @tab-click="Click"
@@ -26,10 +33,9 @@
 </template>
 
 <script>
-import tagTitle from './tagTitle'
 export default {
   name: 'TagCom',
-  components: { tagTitle },
+  // components: { tagTitle },
   props: {
     title: {
       type: String,
@@ -65,9 +71,6 @@ export default {
     Click(tab, event) {
       this.$emit('input', this.chooseName)
       this.$emit('tab-click', tab, event)
-    },
-    handelClick(e) {
-      this.$emit('click-icon', e)
     }
   }
 }
@@ -83,5 +86,24 @@ export default {
   /deep/.el-tabs__active-bar {
     height: 3px;
   }
+}
+
+.header {
+  display: flex;
+  justify-content: space-between;
+}
+.title {
+  font-weight: bold;
+  font-size: 18px;
+  padding: 14px 0 16px;
+  line-height: 28px;
+  color: #202940;
+  .icon-arrow-right-outlined:before {
+    color: #a0a8ae;
+  }
+}
+.button--wr {
+  padding-bottom: 18px;
+  padding-top: 6px;
 }
 </style>
