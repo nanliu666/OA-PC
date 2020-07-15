3a176a23d3222501cafdd9d55ed26a9444d62a28
sutuo <sutuo1988@live.com>
2020-07-15 17:03:13 +0800

    fix: 状态显示

--- src/views/personnel/recruit/specificPage.vue
+++ src/views/personnel/recruit/specificPage.vue
@@ -65,9 +65,9 @@
         </template>
         <el-col :span="3">
           <span class="nodetitle">状态:</span>
-          <span style="color: #207EFA">{{
-            user.needNum !== user.entryNum ? '招聘中' : '已结束'
-          }}</span>
+          <span style="color: #207EFA">
+            {{ translator({ dictKey: 'progress', value: user.progress }) }}
+          </span>
         </el-col>
         <ButtongroupFrom :child-data="childData" />
       </el-row>
@@ -198,6 +198,17 @@ const OVERVIEW_PROPS = [
   ['candidateNum', '候选人数']
 ]
 
+const PROGRESS_DICTS = [
+  {
+    dictKey: 'Approved',
+    dictValue: '招聘中'
+  },
+  {
+    dictKey: 'Finished',
+    dictValue: '已结束'
+  }
+]
+
 export default {
   name: 'StaffList',
   components: {
@@ -245,6 +256,9 @@ export default {
         showIndexColumn: false,
         enableMultiSelect: false,
         enablePagination: true
+      },
+      dictionary: {
+        progress: PROGRESS_DICTS
       }
     }
   },
@@ -280,6 +294,30 @@ export default {
         path: '/personnel/editPerson',
         query: { recruitmentId: this.$route.query.id }
       })
+    },
+
+    translator({ value, dictKey, $config: config }) {
+      if (!(dictKey = dictKey || _.get(config, 'dictKey'))) {
+        return value
+      }
+
+      let dicts = this.dictionary[dictKey]
+      // 如果字典为 undefined 时候加载字典
+      if (!dicts) this.pushDiction(dictKey)
+      let result = value
+      _.each(dicts, (item) => {
+        if (item.dictKey === _.trim(value)) {
+          result = item.dictValue
+          return false
+        }
+      })
+      return result
+    },
+    // 添加字典
+    async pushDiction(dictKey) {
+      const dict = await this.$store.dispatch('CommonDict', dictKey)
+      this.$set(this.dictionary, dictKey, dict)
+      return dict
     }
   }
 }
