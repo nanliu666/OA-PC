49689cbeb3edbee467c2b7e3c24c2803b96c5951
xiangrun <xiangrunkong.epro.com.cn>
2020-07-24 15:47:18 +0800

    mock: 审批接口mock

--- src/mock/apprProcess.js
+++ src/mock/apprProcess.js
@@ -57,11 +57,97 @@ const processListData = {
     }
   ]
 }
+const categoryData = {
+  ...normalData
+}
+const draftData = {
+  ...normalData,
+  response: [
+    {
+      id: 30,
+      name: '未启用',
+      sort: 1,
+      processes: [
+        {
+          processId: '1',
+          processName: '用印申请',
+          status: 0,
+          icon: 'approval-Seal-bicolor',
+          remark: '这里是入职申请的简介',
+          visibleRange: 'all'
+        },
+        {
+          processId: '1',
+          processName: '调岗申请',
+          status: 1,
+          icon: 'approval-Seal-bicolor',
+          remark: '这里是入职申请的简介',
+          visibleRange: 'all'
+        }
+      ]
+    }
+  ]
+}
+const groupData = {
+  ...normalData,
+  response: [
+    {
+      id: 1,
+      name: '人事审批',
+      sort: 1
+    },
+    {
+      id: 30,
+      name: '考勤审批',
+      sort: 1
+    }
+  ]
+}
 export default ({ mock }) => {
   if (!mock) return
   // v2版本接口
   // 审批列表查询，调用接口：审批流程列表查询接口【GET /appr/v2/appr/process/list】
   Mock.mock(new RegExp('/api/appr/v2/appr/process/list' + '.*'), 'get', () => {
     return processListData
+  })
+  // 审批分组新建接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/category' + '.*'), 'post', () => {
+    return categoryData
+  })
+  // 审批分组修改接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/category' + '.*'), 'put', () => {
+    return categoryData
+  })
+  // 审批分组删除接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/category' + '.*'), 'delete', () => {
+    return categoryData
+  })
+  // 审批流程删除接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/del' + '.*'), 'delete', () => {
+    return categoryData
+  })
+  // 审批流程停用接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/stop' + '.*'), 'post', () => {
+    return categoryData
+  })
+  // 未启用审批流程列表查询接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/draft/list' + '.*'), 'get', () => {
+    return draftData
+  })
+  // 审批分组查询接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/category' + '.*'), 'get', () => {
+    return groupData
+  })
+  // 审批流程移动接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/move' + '.*'), 'post', () => {
+    return categoryData
+  })
+  // 审批流程启用接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/start' + '.*'), 'post', () => {
+    return categoryData
+  })
+  // 审批流程启用接口
+  Mock.mock(new RegExp('/api/appr/v2/appr/process/release' + '.*'), 'post', () => {
+    return categoryData
   })
 }
