8e6442c076926dae54b3d67e9ab0de4827c27b32
dwq <d13189432359@163.com>
2020-07-29 17:30:10 +0800

    fix:  工作台和代办审批跳转路径修改

--- src/views/wel/workArea.vue
+++ src/views/wel/workArea.vue
@@ -370,7 +370,7 @@ export default {
       } else if (type === 'Approve') {
         // 审批
         this.$router.push({
-          path: '/approval/appr/apprDetail',
+          path: '/apprProcess/apprDetail',
           query: {
             apprNo: bizId,
             toDoList: true
