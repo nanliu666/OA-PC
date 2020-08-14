b4bb241aeff3248343a0ad7dfdae5d011ba26f57
xiangrun <xiangrunkong.epro.com.cn>
2020-08-14 18:00:13 +0800

    feat: 公告中心路由、mock、api、vuex的相关配置

--- src/api/noticeCenter/noticeCenter.js
+++ src/api/noticeCenter/noticeCenter.js
@@ -1,12 +1,17 @@
-import { get, del, post } from '@/router/axios'
+import { get, del, post, put } from '@/router/axios'
 /**
  * 公告列表查询接口
  */
 export const getNoticeCenterList = (params) => get('/notice/v1/notice/center', params)
 /**
- * 公告详情查询接口
+ * 公告中心详情查询接口，阅读数会加1
  */
-export const getNoticeDeatil = (params) => get('/notice/v1/notice/center/detail', params)
+export const getCenterNoticeDeatil = (params) => get('/notice/v1/notice/center/detail', params)
+/**
+ * 公告列表详情查询接口，单纯查询
+ */
+export const getNoticeDeatil = (params) => get('/notice/v1/notice/detail', params)
+
 /**
  * 公告管理查询接口
  */
@@ -18,4 +23,12 @@ export const delNoticeList = (params) => del('/notice/v1/notice', params)
 /**
  * 公告发布接口
  */
-export const publishNoticeList = (params) => post('/notice/v1/notice/publish', params)
+export const publishNotice = (params) => post('/notice/v1/notice/publish', params)
+/**
+ * 公告创建接口
+ */
+export const creatNotice = (params) => post('/notice/v1/notice', params)
+/**
+ * 公告修改接口
+ */
+export const fixNotice = (params) => put('/notice/v1/notice', params)
