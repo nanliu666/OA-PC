af888d94116728bde0af5378afae6cdab9eba2b7
xiangrun <xiangrunkong.epro.com.cn>
2020-08-14 17:57:58 +0800

    feat: tinymce组件配置

---
+++ src/components/tinymce/src/toolbar.js
@@ -0,0 +1,6 @@
+const toolbar = [
+  'searchreplace bold italic underline strikethrough alignleft aligncenter alignright outdent indent  blockquote undo redo removeformat subscript superscript code codesample fontselect   ',
+  'bullist numlist link charmap anchor pagebreak insertdatetime table emoticons forecolor backcolor fullscreen styleselect uploadimg fontsizeselect image'
+]
+
+export default toolbar
