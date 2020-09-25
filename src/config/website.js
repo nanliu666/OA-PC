4db80ad2cc3d952be4b160dad28f05307a4d165c
liukangqi <liukangqi56@hotmail.com>
2020-09-25 10:47:59 +0800

    feat: 修改秘钥

--- src/config/website.js
+++ src/config/website.js
@@ -6,8 +6,8 @@ export default {
   logo: 'S',
   key: 'saber', //配置主键,目前用于存储
   indexTitle: '百利宏控股',
-  clientId: 'saber', // 客户端id
-  clientSecret: 'saber_secret', // 客户端密钥
+  clientId: 'Admin', // 客户端id
+  clientSecret: 'Admin_PC_2020ePRO', // 客户端密钥
   tenantMode: true, // 是否开启租户模式
   tenantId: '000000', // 管理组租户编号
   captchaMode: true, // 是否开启验证码模式
