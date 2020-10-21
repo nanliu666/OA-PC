d8977a0d233e5052eaa0becfe4be4de632c9c65e
sutuo <tuo.su@epro.com.cn>
2020-10-21 15:38:52 +0800

    feat: 地址控件的值结构修改

--- src/components/location-picker/LocationPicker.vue
+++ src/components/location-picker/LocationPicker.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="location-picker">
     <el-cascader
-      v-model="form.location"
+      v-model="locations"
       class="location-picker__cascader"
       :options="provinceAndCityData"
       placeholder="请选择地址"
@@ -29,9 +29,34 @@ export default {
     return {
       form: {
         ...this.value
-      },
-      provinceAndCityData
+      }
     }
+  },
+
+  computed: {
+    locations: {
+      get() {
+        const { provinceAndCityData } = this
+        let result = []
+        ;(function findCode(arr, locations, target) {
+          _.each(arr, (item) => {
+            if (item.value === target) {
+              locations.push(item)
+              result = locations
+              return false
+            } else if (!_.isEmpty(item.children)) {
+              findCode(item.children, _.concat(locations, item), target)
+            }
+          })
+          return
+        })(provinceAndCityData, [], this.form.location)
+        return result.map(({ value }) => value)
+      },
+      set(val) {
+        this.form.location = _.last(val)
+      }
+    },
+    provinceAndCityData: () => provinceAndCityData
   },
   watch: {
     form: {
--- src/const/provinceAndCityData.js
+++ src/const/provinceAndCityData.js
@@ -2,9 +2,10 @@ import { regionData } from 'element-china-area-data'
 import _ from 'lodash'
 
 const REGION_DATA = _.map(_.cloneDeep(regionData), (region) => {
-  if (_.get(_.head(region.children), 'label') === '市辖区') {
-    region.children = _.concat(...region.children.map((child) => child.children))
-  }
+  // FIX: 使用省市区三级联动的时候不再取消市辖区了
+  // if (_.get(_.head(region.children), 'label') === '市辖区') {
+  //   region.children = _.concat(...region.children.map((child) => child.children))
+  // }
   return region
 })
 
